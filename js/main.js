118abaafc62b1b05d0c44f3289a39de203b0a61e
Stanislaw Wadowski <stanislaw.wadowski@performgroup.com>
2020-09-21 20:39:01 +0200

    change first project to portfolio

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+
+let project1_banner = document.querySelector(".portfolio__project1");
+
+project1_banner.addEventListener('click', function(){
+    window.open("project_card/index.html", "_blank")
+})
